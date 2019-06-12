76155c860541fb50941ff0f5b26944b7451d41e4
AparicioJohan <44405276+AparicioJohan@users.noreply.github.com>
2019-06-12 14:03:04 -0500

    Update AIC.R

--- AIC.R
+++ AIC.R
@@ -1,6 +1,6 @@
 AIC.SpATS <- function(Modelo){
   p <- length(Modelo$coeff[!attr(Modelo$coeff,"random")])   # number of fixed coefficients
-  q <- length(Modelo$var.comp) + 1                          # number of random components + residual variance
+  q <- length(Modelo$var.comp) + 1                          # number of random components + 1 (residual variance)
   lo.lik <- Modelo$deviance/-2                              # log Likelihood 
   aic <- -2*lo.lik + 2*(p+q)                                # Akaike Information Criterion
   return(aic)
