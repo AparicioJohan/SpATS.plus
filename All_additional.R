c908b0528489aeddb6ff21bb09219512361d2846
AparicioJohan <44405276+AparicioJohan@users.noreply.github.com>
2019-09-30 11:12:10 -0500

    Update All_additional.R

--- All_additional.R
+++ All_additional.R
@@ -94,6 +94,7 @@ npars.SpATS <- function(Model){
 
 # Autoplot 
 autoplot.SpATS <- function(Model,...){
+
   response      <- Model$data[,Model$model$response]          # Response Variable
   genotype      <- Model$data[,Model$model$geno$genotype]     # Genotypes
   var.res       <- Model$psi[1]                               # Residual Variance
@@ -103,13 +104,14 @@ autoplot.SpATS <- function(Model,...){
   sqrt.stand    <- sqrt(abs(stand.res))                       # Scale Location
   inf.s         <- data.frame(genotype,response,fitted,residuals,stand.res,sqrt.stand) 
   
-  p1 <- ggplot(inf.s, aes(x=fitted,y=stand.res))+geom_point(...)+ labs(x="Fitted values",y="Standardized residuals", title = "Residuals vs Fitted")
-  p2 <- ggplot(inf.s, aes(sample=stand.res))+stat_qq(...)+stat_qq_line()+labs(x="Theorical Quantile",y="Standardized residuals",title = "Normal Q-Q")
-  p3 <- ggplot(inf.s, aes(x=fitted,y=sqrt.stand))+geom_point(...)+ labs(x="Fitted values",y=expression(sqrt("|Standardized residuals|")), title = "Scale-Location")
-  p4 <- ggplot(inf.s, aes(x=as.numeric(genotype),y=stand.res))+ geom_point(...)+labs(x="Factor Genotype",y="Standardized residuals",title = "Residuals vs Factor levels")
+  p1 <- ggplot(inf.s, aes(x=fitted,y=stand.res))+geom_point(...)+ labs(x="Fitted values",y="Standardized residuals", title = "Residuals vs Fitted")+theme_bw()
+  p2 <- ggplot(inf.s, aes(sample=stand.res))+stat_qq(...)+stat_qq_line()+labs(x="Theorical Quantile",y="Standardized residuals",title = "Normal Q-Q")+theme_bw()
+  p3 <- ggplot(inf.s, aes(x=fitted,y=sqrt.stand))+geom_point(...)+ labs(x="Fitted values",y=expression(sqrt("|Standardized residuals|")), title = "Scale-Location")+theme_bw()
+  p4 <- ggplot(inf.s, aes(x=as.numeric(genotype),y=stand.res))+ geom_point(...)+labs(x="Factor Genotype",y="Standardized residuals",title = "Residuals vs Factor levels")+theme_bw()
   
   return(suppressWarnings(ggarrange(p1,p2,p3,p4)))
 }
+
 
 
 ###########################
